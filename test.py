1f34e06d3b75d6f39c21278b9f06e658d27661d8
Ethan Mahintorabi <ethanmoon@google.com>
2025-10-24 00:19:52 -0700

    getting nfet connected to photodiode

--- test.py
+++ test.py
@@ -4,12 +4,17 @@ import gdsfactory as gf
 import gf180mcu
 
 
-def photo_diode():
+@gf.cell
+def nwell_psub_photodiode(width: int = 5):
+    """Defines a nwell p-substrate photo diode
+
+    Args:
+        width: The width and height of the photo diode in microns
+    """
     # corresponds to 8um in gf180mcu
-    photo_sensitive_width_and_height = 5
     photo_diode_rect = gf.Component()
     photo_diode_nwell = gf.components.rectangle(
-        size = (photo_sensitive_width_and_height, photo_sensitive_width_and_height), 
+        size = (width, width), 
         layer= gf180mcu.LAYER.nwell)
     
     photo_diode_rect << photo_diode_nwell
@@ -18,13 +23,11 @@ def photo_diode():
     router = 100
     n = 300 # points in circle
     # Round corners for all layers.
-    photo_diode_rounded = gf.Component("photo_sensitve_area")
+    photo_diode_rounded = gf.Component()
     for layer, polygons in photo_diode_rect.get_polygons().items():
         for p in polygons:
             p_round = p.round_corners(rinner, router, n)
             photo_diode_rounded.add_polygon(p_round, layer=layer)
-
-    
 
     contact_layer = photo_diode_rounded.add_ref(gf180mcu.cells.via_stack(x_range=(0,0.5), y_range=(0,1)))
     
@@ -34,6 +37,7 @@ def photo_diode():
     nplus.dymax = photo_diode_rounded.dymax - rounded_corner_width_and_height
 
     contact_layer.center = nplus.center
+    photo_diode_rounded.add_ports(contact_layer.ports)
 
     return photo_diode_rounded
 
@@ -43,7 +47,20 @@ gf.kcl.dbu = 5e-3  # set 1 DataBase Unit to 5 nm
 
 gf180mcu.PDK.activate()
 
-component = photo_diode()
-component.pprint_ports()
+parent_com = gf.Component()
 
-component.show()
+
+component = nwell_psub_photodiode()
+parent_com << component
+
+nfet = parent_com << gf180mcu.cells.nfet(w_gate=0.36, l_gate=0.36)
+nfet.center = component.ports["e1"].center
+nfet.dxmin = component.dxmax + 0.4
+
+metal1_diode_con = parent_com << gf.components.rectangle(size = (1.1,component.ports["e1"].width), layer=gf180mcu.LAYER.metal1)
+metal1_diode_con.center = component.ports["e1"].center
+metal1_diode_con.xmin = component.ports["e1"].dx
+
+component.pprint_ports()
+parent_com.pprint_ports()
+parent_com.show()
